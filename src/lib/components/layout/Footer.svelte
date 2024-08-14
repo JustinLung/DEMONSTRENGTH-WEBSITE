e6afa688d826f458027df45d07fcd991e6c33f9d
JustinLung <justinlung77@gmail.com>
2024-08-14 22:04:46 +0200

    updated footer title

--- src/lib/components/layout/Footer.svelte
+++ src/lib/components/layout/Footer.svelte
@@ -1,4 +1,6 @@
 <footer class="container mx-auto p-4">
-	Copyright {new Date().getFullYear()} &#169; Footer &#183; Developed by
-	<a href="https://justinlung.com/" target="_blank" rel="noopener noreferrer">Justin Lung</a>
+  Copyright {new Date().getFullYear()} &#169; Demonstrength &#183; Developed by
+  <a href="https://justinlung.com/" target="_blank" rel="noopener noreferrer"
+    >Justin Lung</a
+  >
 </footer>
