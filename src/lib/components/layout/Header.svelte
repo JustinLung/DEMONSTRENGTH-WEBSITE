cc49ba7f6976722a54de3f1d6901f3d8aa0d6190
JustinLung <justinlung77@gmail.com>
2024-08-14 22:03:19 +0200

    Header made

--- src/lib/components/layout/Header.svelte
+++ src/lib/components/layout/Header.svelte
@@ -1,25 +1,63 @@
 <script lang="ts">
-	const links = [
-		{
-			title: 'Home',
-			href: '/'
-		},
-		{
-			title: 'About',
-			href: '/about'
-		}
-	];
+  import { easeOutExpo } from "$lib/utils/transitions";
+  import { slide } from "svelte/transition";
+
+  let openMenu = false;
+
+  const links = [
+    { title: "Home", href: "/" },
+    { title: "About", href: "/about" },
+    { title: "Coaching", href: "/coaching" },
+    { title: "Contact", href: "/contact" },
+  ];
 </script>
 
 <header class="flex items-center justify-between container mx-auto p-4">
-	<h1 class="text-xl font-bold">DEMONSTRENGTH</h1>
-	<nav>
-		<ul class="flex gap-4">
-			{#each links as link}
-				<li>
-					<a href={link.href}>{link.title}</a>
-				</li>
-			{/each}
-		</ul>
-	</nav>
+  <h1 class="text-xl font-bold relative z-10">DEMONSTRENGTH</h1>
+
+  <nav class="hidden md:block">
+    <ul class="flex gap-4">
+      {#each links as link}
+        <li>
+          <a href={link.href} class="hover:underline">{link.title}</a>
+        </li>
+      {/each}
+    </ul>
+  </nav>
+
+  <button
+    class="block md:hidden relative z-10"
+    on:click={() => {
+      openMenu = !openMenu;
+    }}
+  >
+    {#if openMenu}
+      MENU
+    {:else}
+      CLOSE
+    {/if}
+  </button>
 </header>
+
+{#if openMenu}
+  <nav
+    class="md:hidden p-4 fixed h-screen bg-purple-800 w-full top-0"
+    transition:slide={{ duration: 800, easing: easeOutExpo }}
+  >
+    <ul class="flex flex-col justify-center items-center w-full h-screen gap-4">
+      {#each links as link}
+        <li>
+          <a
+            href={link.href}
+            class="text-[2.5rem] uppercase"
+            on:click={() => {
+              openMenu = false;
+            }}
+          >
+            {link.title}
+          </a>
+        </li>
+      {/each}
+    </ul>
+  </nav>
+{/if}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,4 +2,4 @@
 	import Hero from "$lib/components/shared/Hero.svelte";
 </script>
 
-<Hero title="DEMONSTRENGTH" description="Bring Powerlifting to the next level" src="/assets/images/hero.jpg" alt="Demonstrength Athletes"/>
+<!-- <Hero title="DEMONSTRENGTH" description="Bring Powerlifting to the next level" src="/assets/images/hero.jpg" alt="Demonstrength Athletes"/> -->
