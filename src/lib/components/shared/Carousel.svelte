0a9e13a2089427e0cbf8649b45a78cde793d2c65
JustinLung <justinlung77@gmail.com>
2024-09-06 23:39:35 +0200

    added svg's

--- src/lib/components/shared/Carousel.svelte
+++ src/lib/components/shared/Carousel.svelte
@@ -43,8 +43,54 @@
 	<div class="carousel-header">
 		<h2>{title}</h2>
 		<div class="button-container">
-			<button onclick={prev} disabled={!canPrev}>Previous</button>
-			<button onclick={next} disabled={!canNext}>Next</button>
+			<button onclick={prev} disabled={!canPrev}>
+				<svg
+					width="34"
+					height="20"
+					viewBox="0 0 34 20"
+					fill="none"
+					xmlns="http://www.w3.org/2000/svg"
+				>
+					<path
+						d="M11 2.66666L3.66667 9.99999L11 17.3333"
+						stroke="#FAFAFA"
+						stroke-width="3.66667"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					/>
+					<path
+						d="M31.1667 10L3.66675 10"
+						stroke="#FAFAFA"
+						stroke-width="3.66667"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					/>
+				</svg>
+			</button>
+			<button onclick={next} disabled={!canNext}>
+				<svg
+					width="34"
+					height="20"
+					viewBox="0 0 34 20"
+					fill="none"
+					xmlns="http://www.w3.org/2000/svg"
+				>
+					<path
+						d="M23 2.66666L30.3333 9.99999L23 17.3333"
+						stroke="#FAFAFA"
+						stroke-width="3.66667"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					/>
+					<path
+						d="M2.83325 10L30.3333 10"
+						stroke="#FAFAFA"
+						stroke-width="3.66667"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					/>
+				</svg>
+			</button>
 		</div>
 	</div>
 	<div class="embla" bind:this={emblaNode}>
@@ -83,6 +129,12 @@
 
 					&:disabled {
 						color: var(--grey);
+					}
+
+					svg {
+						path {
+							strokg: var(--white);
+						}
 					}
 				}
 			}
